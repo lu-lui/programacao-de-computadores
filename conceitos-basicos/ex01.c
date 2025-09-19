2093d0008b8ee4d832ac1e9c173a168af0b162d2
lu-lui <luisaneutzlingpassos@gmail.com>
2025-09-19 18:39:11 -0300

    ex01

--- conceitos-basicos/ex01.c
+++ conceitos-basicos/ex01.c
@@ -1,1 +1,15 @@
 /*Escreva um programa para ler o raio de um círculo, calcular e escrever a sua área.*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float raio, area; 
+
+    printf("Digite o valor do raio do circulo: ");
+    scanf("%f", &raio);
+
+    area = 3.14 * pow(raio, 2);
+
+    printf("A area do circulo de raio %.2f e %.2f", raio, area);
+}
