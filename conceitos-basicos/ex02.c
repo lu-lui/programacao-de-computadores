d396645fc76745106a0c5071ceb0a779c5d4a47a
lu-lui <luisaneutzlingpassos@gmail.com>
2025-09-19 18:48:59 -0300

    ex02

--- conceitos-basicos/ex02.c
+++ conceitos-basicos/ex02.c
@@ -1,1 +1,17 @@
 /* 2. Escreva um programa para ler o salário mensal e o percentual de reajuste. Calcular e escrever o valor do novo salário.*/
+
+#include <stdio.h>
+
+int main(){
+    float salario, reajuste, salario_reajustado;
+
+    printf("Digite o valor atual do salario: R$");
+    scanf("%f", &salario);
+
+    printf("Percentual de reajuste:");
+    scanf("%f", &reajuste);
+
+    salario_reajustado = salario * (reajuste / 100 + 1);
+
+    printf("O salario foi reajustado para R$%.2f", salario_reajustado);
+}
