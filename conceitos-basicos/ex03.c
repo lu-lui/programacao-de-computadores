3f94a42c656d26eb01ab11b72d26733274e030c4
lu-lui <luisaneutzlingpassos@gmail.com>
2025-09-19 19:05:25 -0300

    ok

--- conceitos-basicos/ex03.c
+++ conceitos-basicos/ex03.c
@@ -1,1 +1,14 @@
 /*3. Escreva um programa para ler uma temperatura em graus Fahrenheit, calcular e escrever o valor correspondente em graus Celsius. C / 5 = F - 32 / 9*/
+
+#include <stdio.h>
+
+int main(){
+    float Fahrenheit, Celsius;
+
+    printf("Digite uma temperatura em  graus Fahrenheit: ");
+    scanf("%f", &Fahrenheit);
+
+    Celsius = (Fahrenheit - 32) / 9;
+
+    printf("A temperatura correspondente em Celsius e: %.2f", Celsius * 5);
+}
--- conceitos-basicos/ex04.c
+++ conceitos-basicos/ex04.c
@@ -1,1 +1,14 @@
 /*4. Escreva um programa para ler uma temperatura em graus Celsius, calcular e escrever o valor correspondente em graus Fahrenheit. */
+
+#include <stdio.h>
+
+int main(){
+    float Fahrenheit, Celsius;
+
+    printf("Digite uma temperatura em  graus Celsius: ");
+    scanf("%f", &Celsius);
+
+    Fahrenheit = Celsius / 5 * 9;
+
+    printf("A temperatura correspondente em Fahrenheit e: %.2f", Fahrenheit + 32);
+}
