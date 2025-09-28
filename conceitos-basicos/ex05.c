21924fdbc077e6b4f363dd06077552a6e018ffd0
lu-lui <luisaneutzlingpassos@gmail.com>
2025-09-28 15:30:55 -0300

    ok

--- conceitos-basicos/ex05.c
+++ conceitos-basicos/ex05.c
@@ -1,1 +1,29 @@
 /*5. Escreva um programa para ler as dimensões de uma cozinha retangular (comprimento, largura e altura), calcular e escrever a quantidade de caixas de azulejos para se colocar em todas as suas paredes (considere que não será descontado a área ocupada por portas e janelas). Cada caixa de azulejos possui 1,5 m2. */
+
+#include <stdio.h>
+
+int main(){
+    float comprimento, largura, altura, total;
+
+    printf("Dimensões da casa: \nComprimento: ");
+    scanf("%f", &comprimento);
+
+    printf("Altura: ");
+    scanf("%f", &altura);
+
+    printf("Largura: ");
+    scanf("%f", &largura);
+
+    total = altura * largura * comprimento;
+
+    printf("Total: %.2f", total);
+
+    for (int i = total; i < 0; i--){
+        if (total)
+        {
+            /* code */
+        }
+        
+    }
+    
+}
--- conceitos-basicos/ex06.c
+++ conceitos-basicos/ex06.c
@@ -1,1 +1,24 @@
 /*6. Escreva um programa para ler o número de eleitores de um município, o número de votos brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa em relação ao total de eleitores. */
+
+#include <stdio.h>
+
+int main(){
+    int eleitores, brancos, nulos, validos;
+    float porcentagem;
+
+    printf("Numero de eleitores: ");
+    scanf("%d", &eleitores);
+
+    printf("Votos brancos:");
+    scanf("%d", &brancos);
+
+    printf("Votos nulos: ");
+    scanf("%d", &nulos);
+
+    printf("Votos validos: ");
+    scanf("%d", &validos);
+
+    porcentagem = eleitores 
+
+    printf("Porcentagem de votos validos: %f");
+}
