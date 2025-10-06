5a8c2748bd3c0009596fce11f7f5d0180be9c573
lu-lui <luisaneutzlingpassos@gmail.com>
2025-10-05 21:22:22 -0300

    ok

--- estruturas-condicionais/ex21.c
+++ estruturas-condicionais/ex21.c
@@ -1,2 +1,26 @@
-// 21. Escreva um programa que leia o valor de 3 ângulos de um triângulo e escreva se o triângulo é acutângulo, retângulo ou obtusângulo. OBS: triângulo retângulo: possui um ângulo reto. triângulo obtusângulo: possui um ângulo obtuso. triângulo acutângulo: possui 3 ângulos agudos.
+/* 21. Escreva um programa que leia o valor de 3 ângulos de um triângulo e escreva se o triângulo é acutângulo, retângulo ou obtusângulo. OBS: 
+triângulo retângulo: possui um ângulo reto. 
+triângulo obtusângulo: possui um ângulo obtuso. 
+triângulo acutângulo: possui 3 ângulos agudos.*/
+
+#include <stdio.h>
+
+int main(){
+    int angulo[3];
+
+    printf("Descubra o nome do triangulo! \nDigite as inclinacoes dos angulos:\n");
+
+    for (int i = 0; i < 3; i++){
+        printf("Angulo %d: ", i + 1);
+        scanf("%d", &angulo[i]); 
+    }
+    
+    if (angulo[0] == 90 || angulo[1] == 90 || angulo[2] == 90){
+        printf("O triangulo e retangulo");
+    } else if (angulo[0] > 90 || angulo[1] > 90 || angulo[2] > 90){
+        printf("O triangulo e obtusangulo");
+    } else if (angulo[0] < 90 && angulo[1] < 90 && angulo[2] < 90){
+        printf("O triangulo e acutangulo");
+    }
+}
 
--- estruturas-condicionais/ex22.c
+++ estruturas-condicionais/ex22.c
@@ -1,3 +1,32 @@
 /*22. Um mercado está vendendo frutas com a tabela de preços abaixo. Se o cliente comprar mais de 8 Kg em frutas ou o valor total da compra ultrapassar R$ 35,00, receberá ainda um desconto de 20 % sobre esse total. Escreva um programa para ler a quantidade (em Kg) de morangos e a quantidade (em Kg) de maçãs adquiridas e escreva o valor a ser pago pelo cliente.
     Morango: Até 5 Kg - R$ 5,00 p/Kg;  Acima de 5 Kg - R$ 4,00 p/Kg
     Maçã: Até 5 Kg - R$ 3,00 p/Kg; Acima de 5 Kg - R$ 2,00 p/Kg*/
+
+    #include <stdio.h>
+
+    int main(){
+        float kg, soma_preco = 0, soma_kg = 0;
+    
+        printf("Kg de morango vendidos: ");
+        scanf("%f", &kg);
+        soma_kg += kg; 
+
+        if (kg <= 5)
+            soma_preco += kg * 5;
+        else
+            soma_preco += kg * 4;
+
+        printf("Kg de maca vendidos: ");
+        scanf("%f", &kg);
+        soma_kg += kg; 
+
+        if (kg <= 5)
+            soma_preco += kg * 3;
+        else
+            soma_preco += kg * 2;
+
+        if (soma_kg > 8 || soma_preco > 35)
+            printf("Valor da compra: R$%.2f", soma_preco * 0.8);
+        else
+            printf("Valor da compra: R$%.2f", soma_preco);
+    }
