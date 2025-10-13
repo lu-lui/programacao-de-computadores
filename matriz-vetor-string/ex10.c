e4b04951dd154d90c30f8b47f735041a9e627bf2
lu-lui <luisaneutzlingpassos@gmail.com>
2025-10-13 10:24:24 -0300

    ok

--- matriz-vetor-string/ex10.c
+++ matriz-vetor-string/ex10.c
@@ -1,2 +1,31 @@
 // Ler um vetor M de 10 elementos. Troque a seguir o 1o. elemento com o 6o., o 2o. com o 7o.,etc. até o 5o. com 10o. e escreva o vetor M assim modificado. 
 
+#include <stdio.h>
+
+int main(){
+    int M[10]; 
+
+    printf("Digite valores do vetor U: \n");
+    
+    for (int i = 0; i < 10; i++){
+        printf("Posicao %d: ", i);
+        scanf("%d", &M[i]);
+    }
+
+    printf("Vetor original: ");
+    for (int i = 0; i < 10; i++){
+        printf("%d ", M[i]);
+    }
+
+    for (int i = 0; i < 5; i++){
+        int temp;
+        temp = M[i];
+        M[i] = M[i+5];
+        M[i+5] = temp;
+    }
+    
+    printf("\nVetor modificado: ");
+    for (int i = 0; i < 10; i++){
+        printf("%d ", M[i]);
+    }
+}
