22f0d58415a321bd373a95f8dd02d650a3e66215
lu-lui <luisaneutzlingpassos@gmail.com>
2025-11-09 16:12:27 -0300

    ok

--- repeticao/ex14.c
+++ repeticao/ex14.c
@@ -6,10 +6,10 @@ int main(){
     int i=1, j=1;
 
     for ( i = 1; i <= 10; i++){
-        for ( j = 0; j <= 10; i++){
+        for ( j = 1; j <= 10; j++){
             printf("%d X %d = %d \n", i, j, i * j);
         }
-        
+        printf("\n");
     }
     
     return 0;
