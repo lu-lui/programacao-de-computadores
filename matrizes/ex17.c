a5a1925c525af153958f3483b4c0aa23ddf0813d
lu-lui <lpneutzling@gmail.com>
2026-01-12 15:27:28 -0300

    teste

--- matrizes/ex17.c
+++ matrizes/ex17.c
@@ -1,2 +1,7 @@
 // 17. Ler uma matriz n x n que representa a distância existente entre n cidades entre si. A seguir ler um vetor de tamanho m, com os códigos das cidades de um trajeto. Escrever a distância total percorrida. 
 
+int main(){
+
+    
+    return 0;
+}
