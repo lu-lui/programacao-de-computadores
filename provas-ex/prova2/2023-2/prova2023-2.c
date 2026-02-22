473e7c16896c5c4854a26e691748651dbc7bedad
lu-lui <lpneutzling@gmail.com>
2026-02-22 10:39:39 -0300

    pronto, falta ordenar

--- provas-ex/prova2/2023-2/prova2023-2.c
+++ provas-ex/prova2/2023-2/prova2023-2.c
@@ -5,3 +5,95 @@ O que será avaliado?
 2. A leitura da quantidade indeterminada de caracteres do arquivo (valor: 3 pontos)
 3. A alocação de cada célula e a inserção em ordem nas listas (valor: 3 pontos)
 4. A gravação dos caracteres no arquivo (valor: 2 pontos)*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct abc{
+    char letra;
+    struct abc *prox;
+};
+
+typedef struct abc alfabeto;
+
+alfabeto *cria();
+void carrega(alfabeto *vogais, alfabeto *consoantes);
+void salva(alfabeto *vogais, alfabeto *consoantes);
+
+int main(){
+    alfabeto *vogais, *consoantes;
+
+    vogais = cria();
+    consoantes = cria();
+    carrega(vogais, consoantes);
+    salva(vogais, consoantes);
+
+    return 0;
+}
+
+
+alfabeto *cria(){
+    alfabeto *lista;
+
+    lista = (alfabeto *)malloc(sizeof(alfabeto));
+    lista->prox = NULL;
+    
+    return lista;
+}
+
+void carrega(alfabeto *vogais, alfabeto *consoantes){
+    FILE *arquivo;
+    alfabeto *novo, *p; 
+
+    arquivo = fopen("entrada.txt", "r");
+        if(arquivo == NULL){
+            printf("Erro ao abrir arquivo\n");
+            return;
+}
+
+    while (1){
+        novo = (alfabeto *)malloc(sizeof(alfabeto));
+        if(novo == NULL)
+            break;
+
+        if(fscanf(arquivo, " %c", &novo->letra) != 1){
+            free(novo);
+            break;
+        }
+    
+
+        novo->prox = NULL;
+            
+        if(novo->letra == 'A' || novo->letra == 'E'|| novo->letra == 'I' || novo->letra == 'O' || novo->letra == 'U'){
+           p = vogais;
+        } else p = consoantes;
+        
+        while (p->prox != NULL){
+            p = p->prox;
+        }
+        p->prox = novo;
+
+    }
+
+    fclose(arquivo);
+}
+
+void salva(alfabeto *vogais, alfabeto *consoantes){
+    FILE *arquivo;
+    alfabeto *p;
+
+    arquivo = fopen("saida.txt", "w");
+
+    fprintf(arquivo, "Vogais:\n");
+    for(p = vogais->prox; p != NULL; p = p->prox){
+        fprintf(arquivo, "%c\n", p->letra);
+    }
+
+    fprintf(arquivo, "Consoantes::\n");
+    for(p = consoantes->prox; p != NULL; p = p->prox){
+        fprintf(arquivo, "%c\n", p->letra);
+    }
+
+    fclose(arquivo);
+}
