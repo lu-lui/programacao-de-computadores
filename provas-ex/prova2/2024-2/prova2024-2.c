bc1f83d1b935400790763596e61033a745a6a63c
lu-lui <lpneutzling@gmail.com>
2026-02-24 20:56:55 -0300

    prova

--- provas-ex/prova2/2024-2/prova2024-2.c
+++ provas-ex/prova2/2024-2/prova2024-2.c
@@ -56,13 +56,13 @@ struct pragas *cria(void){
 void imprime(struct pragas *lista){
     struct pragas *novo;
 
-    for(novo = lista->prox; novo != NULL; novo = novo->prox);
+    for(novo = lista->prox; novo != NULL; novo = novo->prox){
         printf("%s\n%s\n%s\n%d\n%d\n%d\n%s\n", novo->nome_popular, novo->nome_cientifico, novo->culturas, novo->quantidade, novo->temperatura_min, novo->temperatura_max, novo->condicoes);
-    
+    }
 }
 
 void carrega(struct pragas *lista){
-    struct pragas *novo;
+    struct pragas *novo, *anterior, *atual;
     FILE *arquivo;
 
     arquivo = fopen("dados.txt", "r");
@@ -73,18 +73,24 @@ void carrega(struct pragas *lista){
 
     while(1){
         novo = (struct pragas *)malloc(sizeof(struct pragas));
-        if(novo == NULL){
+        if(novo == NULL)
             break;
-        }
 
         if(fscanf(arquivo, " %199[^\n]\n%199[^\n]\n%199[^\n]\n%d\n%d\n%d\n%199[^\n]\n", novo->nome_popular, novo->nome_cientifico, novo->culturas, &novo->quantidade, &novo->temperatura_min, &novo->temperatura_max, novo->condicoes) != 7){
             free(novo);
             break;
         }
 
-        novo->prox = lista->prox;
-        lista->prox = novo;
-        
+        anterior = lista;
+        atual = lista->prox;
+
+        while((atual != NULL) && (strcmp(atual->nome_popular, novo->nome_popular) < 0)){
+            anterior = atual;
+            atual = atual->prox;
+        }
+
+        novo->prox = atual;
+        anterior->prox = novo;
     }
 
     fclose(arquivo);
