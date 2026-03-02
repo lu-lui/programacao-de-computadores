26eba57042a811623948408f07932925ba085c58
lu-lui <lpneutzling@gmail.com>
2026-03-02 14:31:23 -0300

    exercicios

--- matrizes/ex25.c
+++ matrizes/ex25.c
@@ -1,3 +1,5 @@
+// 25. Escreva um programa para ler 10 palavras (considere que cada palavra possui no máximo 30 caracteres). Contar e escrever a frequência de cada caractere presente nestas palavras. 
+
 #include <stdio.h>
 
 int main(){
