29358c25f4690b1055b561dc9290c92f7b9e545a
lu-lui <lpneutzling@gmail.com>
2026-03-05 19:14:44 -0300

    exercicios

--- repeticao/ex15.c
+++ repeticao/ex15.c
@@ -1,2 +1,22 @@
 //15. Escreva um algoritmo que forneça quantos números devem ser somados em sequência a partir do 1 (1,2,3,4, ...) de modo que não ultrapasse um valor n informado pelo usuário. 
   
+#include <stdio.h>
+
+int main(){
+    int n, c=0, s=0;
+
+    printf("Digite um número: ");
+    scanf("%d", &n);
+
+   for (int i = 1; s < n; i++){
+        s += i;
+        if(s>n)
+            break;
+
+        c++;
+    }
+    
+    printf("%d números devem ser somados em sequência para se chegar a %d\n", c, n);
+
+    return 0;
+}
--- repeticao/ex16.c
+++ repeticao/ex16.c
@@ -1,2 +1,26 @@
 //16. Ler um valor A e um valor N. Imprimir a soma dos N números inteiros a partir de A(inclusive). Caso N seja negativo ou ZERO, deverá ser lido um novo N(apenas N). 
- 
+
+#include <stdio.h>
+
+int main(){
+    int A, N, s = 0;
+
+    printf("Valor A: ");
+    scanf("%d", &A);
+
+    printf("Valor N: ");
+    scanf("%d", &N);
+    while (N <= 0){
+        printf("Valor invalido\nValor N: ");
+        scanf("%d", &N);
+    }
+
+    for (int i = 0; i < N; i++){
+        printf("%d ", A + i);
+        s += A + i;
+    }
+   
+    printf("\nSoma = %d\n", s);
+
+    return 0;
+}
